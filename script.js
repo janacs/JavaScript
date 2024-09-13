7946b6d31927f511762a5ec15e1034cdbe882e8d
Janderson <jandersonsouza_phn@hotmail.com>
2024-09-13 11:33:51 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,31 @@
+console.log("Hello world");
+
+//Comentario de apenas 1 linha no JS
+/* 
+Comentario de varias linhas no JS
+*/
+
+// Variaveis lugar onde iremos guardar informacoes
+// Declarando variaveis: var, let e const
+
+// Variavel + nome da variavel + = + valor;
+// Criando nomes de variaveis:
+// Não podemos usar nomes com espaços
+// Podemos usar camelCase - escrever duas ou mais palavras juntas, primeira palavra vai iniciar com a letra minuscula, seguida das outras palavras com a primeira letra maiuscula.
+//case sensitive # Letras minusculas de letras maiusculas
+// string "texto"
+
+// let so consegue reatribuir o valor
+let nomeDoAluno = "Blá blá blá";
+nomeDoAluno = "Novo conteúdo" ;
+nomeDoAluno = "Outro teste"
+console.log(nomeDoAluno)
+
+//var você consegue reescrever e também reatribuir
+var pessoa ="Zero Um" ;
+var pessoa = "Zero Dois"
+console.log(pessoa)
+
+//constante nao pode ser alterada
+const treinamento = "JavaScript";
+// Constante não muda
